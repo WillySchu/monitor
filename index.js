aed4cc5f7548de52b2bea985d6a68e3a58b2bddf
Will Schumacher <meintodengel@gmail.com>
2017-02-02 14:36:52 -0800

    update

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const checkGA = require('./rs.js').checkGA;
 const checkInsights = require('./rs.js').checkInsights;
 
 function alert(arg) {
-  // send an alert somewhere
-  console.log(arg)
+  if (arg === true) return
+
+  // send some alert here
 }
 mg = new Monitor(1000, checkGA, alert);
 ml = new Monitor(1000, checkInsights, alert);
